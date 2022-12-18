79d96199b686e15f31d92533ae1b3874dc655d5e
aurelien adoue <aurelien.adoue@gmail.com>
2022-12-18 21:39:26 +0100

    Entry17 : adding len_printed conditions and writing everywhere

--- Printf/ft_printf.c
+++ Printf/ft_printf.c
@@ -13,36 +13,67 @@
 #include <stdio.h>
 #include "printf.h"
 
-static void	print_integer(int n)
+static int	print_integer(int n)
 {
+	int	i;
+	char	*var;
+
+	i = 0;
+	var = NULL;
 	if (n < 0)
 	{
-		ft_putchar('-');
+		i += write(1, "-", 1);
 		n = -n;
 	}
 	if (n >= 10)
 		print_integer(n / 10);
-	ft_putchar('0' + n % 10);
+	var[0] = '0' + n % 10;
+	i += write(1, &var, 1);
+	return (i);
 }
 
-static void	print_unsigned_integer(unsigned int n)
+static int	print_unsigned_integer(unsigned int n)
 {
+	int	i;
+	char	*var;
+
+	i = 0;
+	var = NULL;
 	if (n >= 10)
 		print_unsigned_integer(n / 10);
-	ft_putchar('0' + n % 10);
+	var[0] = '0' + n % 10;
+	i += write(1, &var, 1);
+	return (i);
 }
 
-static void	print_hexadecimal(unsigned int n, char maj)
+static int	print_hexadecimal(unsigned int n, char maj, char p)
 {
+	int	i;
+	char	*var;
+
+	i = 0;
+	var = NULL;
+	if (p == 'p')
+		i += write(1, "0x", 2);
 	if (n >= 16)
-		print_hexadecimal(n / 16);
+		print_hexadecimal((n / 16), maj, p);
 	if (n % 16 < 10)
-		ft_putchar('0' + n % 16);
+	{
+		var[0] = '0' + n % 16; 
+		i += write(1, &var, 1);
+	}
 	else
 		if (maj == 'X')
-			ft_putchar('A' + n % 16 - 10);
+		{
+			var[0] = 'A' + n % 16 - 10;
+			i += write(1, &var, 1);
+		}
 		if (maj == 'x')
-			ft_putchar('a' + n % 16 - 10);
+		{
+			var[0] = 'a' + n % 16 - 10;
+			i += write(1, &var, 1);
+		}
+	return (i);
 }		
 
 int	ft_printf(const char *conv, ...)
@@ -55,45 +86,28 @@ int	ft_printf(const char *conv, ...)
 	len_printed = 0;
 	while (*conv)
 	{
-		if (*conv == "%")
+		if (*conv == '%')
 		{
 			conversion = *(++conv); //?rm conversion and direct use of *conv ?
 			if (conversion == 'c') //one char
-			{
-				ft_putchar(va_arg(ptr, int));
-    				len_printed++;
-			}
+				len_printed += write(1, va_arg(ptr, int), 1); //putchar and putstr in write
 			else if (conversion == 's') //string
-			{
-				len_printed = len_printed + ft_strlen(conv);
-				ft_putstr(va_arg(ptr, const char*));
-			}	
+				len_printed += write(1, va_arg(ptr, const char*), ft_strlen(conv));
     			else if (conversion == 'p') //void * arg in hexa
-			{
-				void *p = va_arg(ptr, void*);
-				ft_putstr("0x");
-				print_hexadecimal((unsigned long) p, 'x');
-			}
+				len_printed += print_hexadecimal((unsigned long) va_arg(ptr, void*), 'x', 'p'); //void *p = va_arg(ptr, void*);
 			else if (conversion == 'd' || conversion == 'i') //nb decimal ou int base 10
-				print_integer(va_arg(ptr, int));
+				len_printed += print_integer(va_arg(ptr, int));
     			else if (conversion == 'u') //unsigned nb decimal base 10
-				print_unsigned_integer(va_arg(ptr, unsigned int));
+				len_printed += print_unsigned_integer(va_arg(ptr, unsigned int));
 			else if (conversion == 'x' || conversion == 'X') //nb in hexa in min/maj
-                                print_hexadecimal(va_arg(ptr, unsigned int), conversion);
-    			else if (conversion == "%") //just a %
-			{				
-				ft_putchar('%');
-				len_printed++;
-			}
+                                len_printed += print_hexadecimal(va_arg(ptr, unsigned int), conversion, 'z');
+    			else if (conversion == '%') //just a %
+				len_printed += write(1, "%", 1);
 			else
-			{
-				ft_putchar('%');
-				ft_putchar(conversion);
-				len_printed = len_printed + 2;
-			}
+				len_printed += write(1, "%", 1);
 		}
 		else
-			ft_putchar(*conv)
+			len_printed += write(1, &conv, 1);
 		conv++;
 	}
 	va_end(ptr);
@@ -102,13 +116,13 @@ int	ft_printf(const char *conv, ...)
 
 int	main ()
 {
-	ft_printf("Hello")
-	printf("%i\n", printf("%i\n", -5));
+	ft_printf("%Hello");
+	//printf("%i\n", printf("%i\n", -5));
 	return (0);
 }
 
-https://www.gnu.org/software/libc/manual/html_node/Why-Variadic.html
-https://github.com/paulo-santana/ft_printf_tester
-https://github.com/Tripouille/printfTester
+//https://www.gnu.org/software/libc/manual/html_node/Why-Variadic.html
+//https://github.com/paulo-santana/ft_printf_tester
+//https://github.com/Tripouille/printfTester
 
-type va_arg (va_list ptr, type) return the value of the next arg and modify ptr to be on the next arg
+//type va_arg (va_list ptr, type) return the value of the next arg and modify ptr to be on the next arg
