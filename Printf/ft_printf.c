4b9c42d0176c18cc901af979e1fd421b1f39dd79
Aurelien Adoue <aadoue@1E7.42angouleme.fr>
2022-12-26 17:39:06 +0100

    Entry20 : Greening the tests with some adjustments and new usage of static var, it's almost fully working

--- Printf/ft_printf.c
+++ Printf/ft_printf.c
@@ -6,140 +6,133 @@
 /*   By: aadoue <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 13:21:38 by aadoue            #+#    #+#             */
-/*   Updated: 2022/12/25 16:56:42 by aadoue           ###   ########.fr       */
+/*   Updated: 2022/12/26 17:27:53 by aadoue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
+#include <limits.h>
+#include <stdio.h>
 #include "ft_printf.h"
+
+static int count = 0;
 
 static int	ft_putchar_return(char c)
 {
-	int	i;
-
-	i = 0;
-	write(1, &c, 1);
-	i++;
-	return (i);
+	count += write(1, &c, 1);
+	return (count);
 }
 
 static int	ft_putstr_return(char *s)
 {
 	int	i;
-	int retour;
 	
 	i = 0;
-	retour = 0;
 	if (!s)
 	{
-		write(1, "(null)", 6);
-		return (6);
+		count += write(1, "(null)", 6);
+		return (count);
 	}
 	while (s[i])
 	{
-		retour += ft_putchar_return(s[i]);
+		ft_putchar_return(s[i]);
 		i++;
 	}
-	return (retour);
+	return (count);
 }
 
 static int	print_integer(int n)
 {
-	int	i;
+	long	nb;
 
-	i = 0;
-	if (n < 0)
+	nb = n;
+	if (nb < 0)
 	{
-		i += ft_putchar_return('-');
-		n = -n;
+		ft_putchar_return('-');
+		nb = -nb;
 	}
-	if (n >= 10)
-		print_integer(n / 10);
-	i += ft_putchar_return('0' + n % 10);
-	return (i);
+	if (nb >= 10)
+		print_integer(nb / 10);
+	ft_putchar_return('0' + nb % 10);
+	return (count);
 }
 
 static int	print_unsigned_integer(unsigned int n)
 {
-	int	i;
-
-	i = 0;
 	if (n >= 10)
 		print_unsigned_integer(n / 10);
-	i += ft_putchar_return('0' + n % 10);
-	return (i);
+	ft_putchar_return('0' + n % 10);
+	return (count);
 }
 
-static int	print_hexadecimal(unsigned long n, char maj)
+static int	print_hexadecimal(unsigned long n, char maj, int zero, char p)
 {
-	int	i;
-
-	i = 0;
+	if (n == 0 && p == 'p')
+	{
+		count += write(1, "(nil)", 5);
+		return (count);
+	}
+	if (zero == 0 && n < 16)
+		count += write(1, "0x", 2);
 	if (n >= 16)
-		print_hexadecimal((n / 16), maj);
+		print_hexadecimal((n / 16), maj, zero, p);
 	if (n % 16 < 10)
-		i += ft_putchar_return('0' + n % 16);
+		ft_putchar_return('0' + n % 16);
 	else
 	{
 		if (maj == 'X')
-			i += ft_putchar_return('A' + n % 16 - 10);
+			ft_putchar_return('A' + n % 16 - 10);
 		if (maj == 'x')
-			i += ft_putchar_return('a' + n % 16 - 10);
+			ft_putchar_return('a' + n % 16 - 10);
 	}
-	return (i);
+	return (count);
 }		
 
 int	ft_printf(const char *conv, ...)
 {
-	int	len_printed;
 	va_list ptr;
-
+	
 	va_start(ptr, conv);
-	len_printed = 0;
 	while (*conv)
 	{
 		if (*conv == '%')
 		{
 			conv++;
 			if (*conv == 'c')
-				len_printed += ft_putchar_return(va_arg(ptr, int));
+				ft_putchar_return(va_arg(ptr, int));
 			else if (*conv == 's')
-				len_printed += ft_putstr_return(va_arg(ptr, void*));
+				ft_putstr_return(va_arg(ptr, void*));
     		else if (*conv == 'p')
-				{
-				len_printed += write(1, "0x", 2);			
-				len_printed += print_hexadecimal((unsigned long) va_arg(ptr, void*), 'x');
-				}			
+					print_hexadecimal((unsigned long) va_arg(ptr, void*), 'x', 0, *conv);
 			else if (*conv == 'd' || *conv == 'i')
-				len_printed += print_integer(va_arg(ptr, int));
+				print_integer(va_arg(ptr, int));
     		else if (*conv == 'u')
-				len_printed += print_unsigned_integer(va_arg(ptr, unsigned int));
+				print_unsigned_integer(va_arg(ptr, unsigned int));
 			else if (*conv == 'x' || *conv == 'X')
-				len_printed += print_hexadecimal(va_arg(ptr, unsigned long), *conv);
+				print_hexadecimal(va_arg(ptr, unsigned long), *conv, 1, *conv);
    			else if (*conv == '%')
-				len_printed += write(1, "%", 1);
+				count += write(1, "%", 1);
 			else
 				{
-				len_printed += write(1, "%", 1);
-				len_printed += ft_putchar_return(*conv);
+				count += write(1, "%", 1);
+				ft_putchar_return(*conv);
 				}
 		}
 		else
-			len_printed += ft_putchar_return(*conv);
+			ft_putchar_return(*conv);
 		conv++;
 	}
 	va_end(ptr);
-	return (len_printed);
+	return (count);
 }
 
-//int	main ()
-//{
-//	//char c[100];
-//	printf("%i\n", ft_printf("%s", (void*)0));
-//	//printf("\n");
-//	printf("%i\n", printf("%s", (void*)0));
-//	return (0);
-//}
+int	main ()
+{
+	//char c[100];
+	printf("%i\n", ft_printf(" %x ", 4294967296));
+	//printf("\n");
+	printf("%i\n", printf(" %x ", 4294967296));
+	return (0);
+}
 
 //gestion des erreurs :
 //invalid format specifiers = %f ou %l... = print("Error: invalid format specifier\n")
